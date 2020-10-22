2e9e75f515c3b19fafb599157f6b2277f6df48a4
Васильев Николай <krankenmorder@gmail.com>
2020-10-22 23:13:03 +0500

    version 1.3
    Определение класса-наследника. Создание объекта класса-наследника. Уничтожение объекта класса-наследника. Добавление паузы для более понятного порядка вызова конструкторов.

--- LabOOP_2/LabOOP_2.cpp
+++ LabOOP_2/LabOOP_2.cpp
@@ -63,6 +63,29 @@ void Point2::reset() { //реализация метода после опред
     b = 0;
 }
 
+class ColorPoint: public Point { //класс-наследник класса Point
+public:
+    int color;
+    ColorPoint(): Point(){ //конструктор по умолчанию
+        printf("Вызвался конструктор ColorPoint по умолчанию.\n");
+        color = 0;
+    }
+    ColorPoint(int x, int y, int color): Point(x, y){ //конструктор с параметрами
+        printf("Вызвался конструктор ColorPoint с параметрами.\n");
+        this->color = color;
+    }
+    ColorPoint(ColorPoint& P) { //конструктор копирования
+        printf("Вызвался конструктор ColorPoint копирования.\n");
+        this->x = P.x;
+        this->y = P.y;
+        this->color = P.color;
+    }
+    ~ColorPoint() { //деструктор Point
+        printf("Вызвался деструктор ColorPoint.\nВходные данные: ");
+        printf("%d, %d, color = %d\n\n", x, y, color);
+    }
+};
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
@@ -71,16 +94,34 @@ int main()
     Point P2(10,10);
     Point P3(P2);
 
+    system("pause");
+    printf("\n");
+
     Point *dinP = new Point; //динамически созданные объекты
     Point *dinP2 = new Point(11, 11);
     Point *dinP3 = new Point(*dinP2);
+
+    system("pause");
+    printf("\n");
 
     delete dinP; //удаление динамически созданных объектов
     delete dinP2;
     delete dinP3;
 
+    system("pause");
+    printf("\n");
+
     Point2 *ab = new Point2(12, 12); //реализация типов видимости
     ab->reset();
     ab->move(2,3);
     delete ab;
+
+    system("pause");
+    printf("\n");
+
+    ColorPoint *colP = new ColorPoint(1, 2, 30); //реализация класса-потомка 
+    delete colP;
+
+    system("pause");
+    printf("\n");
 }
